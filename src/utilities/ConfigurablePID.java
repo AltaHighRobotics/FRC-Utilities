ce873afc9ddff10106a72be2e07de5cf19e16e8c
Braden Byrnes <56095224+Cairezn@users.noreply.github.com>
2022-05-07 19:35:05 -0600

    updated src file to be up to date with new changes

--- src/utilities/ConfigurablePID.java
+++ src/utilities/ConfigurablePID.java
@@ -1,266 +1,296 @@
 package utilities;
 
-//import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.MathUtil;
 
-public class ConfigurablePID {
-  private double proportionalGain = 0;
-  private double integralGain = 0;
-  private double derivitiveGain = 0;
+public class ConfigurablePID
+{
+	private double proportionalGain = 0;
+	private double integralGain = 0;
+	private double derivitiveGain = 0;
 
-  private double maxProportional = 0;
-  private double maxIntegral = 0;
-  private double maxDerivitive = 0;
+	private double maxProportional = 0;
+	private double maxIntegral = 0;
+	private double maxDerivitive = 0;
 
-  private double minOutput = 0;
-  private double maxOutput = 0;
+	private double minOutput = 0;
+	private double maxOutput = 0;
 
-  private double proportional = 0;
-  private double integral = 0;
-  private double derivitive = 0;
+	private double proportional = 0;
+	private double integral = 0;
+	private double derivitive = 0;
 
-  private double currentError = 0;
-  private double pastError = 0;
-  private double errorDelta = 0;
+	private double currentError = 0;
+	private double pastError = 0;
+	private double errorDelta = 0;
 
-  private double output = 0;
+	private double output = 0;
 
-  private double speed = 1;
+	private double speed = 1;
 
-  public ConfigurablePID(double proportionalGain, double integralGain, double derivitiveGain, double maxProportional, double maxIntegral, double maxDerivitive, double minOutput, double maxOutput, double speed) 
-  {
-    this.proportionalGain = proportionalGain;
-    this.integralGain = integralGain;
-    this.derivitiveGain = derivitiveGain;
+	public ConfigurablePID(double proportionalGain, double integralGain, double derivitiveGain, double maxProportional,
+			double maxIntegral, double maxDerivitive, double minOutput, double maxOutput, double speed)
+	{
+		this.proportionalGain = proportionalGain;
+		this.integralGain = integralGain;
+		this.derivitiveGain = derivitiveGain;
 
-    this.maxProportional = maxProportional;
-    this.maxIntegral = maxIntegral;
-    this.maxDerivitive = maxDerivitive;
+		this.maxProportional = maxProportional;
+		this.maxIntegral = maxIntegral;
+		this.maxDerivitive = maxDerivitive;
 
-    this.minOutput = minOutput;
-    this.maxOutput = maxOutput;
+		this.minOutput = minOutput;
+		this.maxOutput = maxOutput;
 
-    this.speed = speed;
-  }
+		this.speed = speed;
+	}
 
-  public double runPID(double setpoint, double processVariable) {
+	public double runPID(double setpoint, double processVariable)
+	{
+		this.currentError = setpoint - processVariable;
+		this.errorDelta = this.currentError - this.pastError;
+		this.pastError = this.currentError;
 
-    this.currentError = setpoint - processVariable;
-    this.errorDelta = this.currentError - this.pastError;
-    this.pastError = this.currentError;
+		this.proportional = MathUtil.clamp(this.currentError * this.proportionalGain, -this.maxProportional,
+				this.maxProportional);
+		this.integral = MathUtil.clamp(this.integral + (this.currentError * this.integralGain), -this.maxIntegral,
+				this.maxIntegral);
+		this.derivitive = MathUtil.clamp(this.errorDelta * this.derivitiveGain, -this.maxDerivitive,
+				this.maxDerivitive);
 
-//    this.proportional = MathUtil.clamp(this.currentError * this.proportionalGain, -this.maxProportional, this.maxProportional);
-//    this.integral = MathUtil.clamp(this.integral + (this.currentError * this.integralGain), -this.maxIntegral, this.maxIntegral);
-//    this.derivitive = MathUtil.clamp(this.errorDelta * this.derivitiveGain, -this.maxDerivitive, this.maxDerivitive);
-//
-//    this.output = MathUtil.clamp(this.proportional + this.integral + this.derivitive, this.minOutput, this.maxOutput);
+		this.output = MathUtil.clamp(this.proportional + this.integral + this.derivitive, this.minOutput,
+				this.maxOutput);
 
-    return this.output;
-  }
+		return this.output;
+	}
 
-  public double runVelocityPID(double setpoint, double processVariable, double processVariableVelocity) {
-    
-    this.currentError = (setpoint - processVariable) * this.speed;
-    this.currentError = this.currentError - processVariableVelocity;
-    this.errorDelta = this.currentError - this.pastError;
-    this.pastError = this.currentError;
-//
-//    this.proportional = MathUtil.clamp(this.currentError * this.proportionalGain, -this.maxProportional, this.maxProportional);
-//    this.integral = MathUtil.clamp(this.integral + (this.currentError * this.integralGain), -this.maxIntegral, this.maxIntegral);
-//    this.derivitive = MathUtil.clamp(this.errorDelta * this.derivitiveGain, -this.maxDerivitive, this.maxDerivitive);
-//
-//    this.output = MathUtil.clamp(this.proportional + this.integral + this.derivitive, this.minOutput, this.maxOutput);
+	public double runVelocityPID(double setpoint, double processVariable, double processVariableVelocity)
+	{
+		this.currentError = (setpoint - processVariable) * this.speed;
+		this.currentError = this.currentError - processVariableVelocity;
+		this.errorDelta = this.currentError - this.pastError;
+		this.pastError = this.currentError;
+		this.proportional = MathUtil.clamp(this.currentError * this.proportionalGain, -this.maxProportional,
+				this.maxProportional);
+		this.integral = MathUtil.clamp(this.integral + (this.currentError * this.integralGain), -this.maxIntegral,
+				this.maxIntegral);
+		this.derivitive = MathUtil.clamp(this.errorDelta * this.derivitiveGain, -this.maxDerivitive,
+				this.maxDerivitive);
 
-    return this.output;
-  }
+		this.output = MathUtil.clamp(this.proportional + this.integral + this.derivitive, this.minOutput,
+				this.maxOutput);
 
-  /**
-   * Get the speed used in runVelocityPID().
-   *
-   * @return speed
-   */
-  public double getSpeed() {
-    return this.speed;
-  }
+		return this.output;
+	}
 
-  /**
-   * Set the speed used in runVelocityPID().
-   *
-   * @param newSpeed the speed to use in runVelocityPID()
-   */
-  public void setSpeed(double newSpeed) {
-    this.speed = newSpeed;
-  }
+	/**
+	 * Get the speed used in runVelocityPID().
+	 *
+	 * @return speed
+	 */
+	public double getSpeed()
+	{
+		return this.speed;
+	}
 
-  /**
-   * Get the minimum allowed output for the controller.
-   *
-   * @return minimum output
-   */
-  public double getMinOutput() {
-    return this.minOutput;
-  }
+	/**
+	 * Set the speed used in runVelocityPID().
+	 *
+	 * @param newSpeed the speed to use in runVelocityPID()
+	 */
+	public void setSpeed(double newSpeed)
+	{
+		this.speed = newSpeed;
+	}
 
-  /**
-   * Set the minimum allowed output for the controller.
-   *
-   * @param newMinOutput the minimum output of the controller
-   */
-  public void setMinOutput(double newMinOutput) {
-    this.minOutput = newMinOutput;
-  }
+	/**
+	 * Get the minimum allowed output for the controller.
+	 *
+	 * @return minimum output
+	 */
+	public double getMinOutput()
+	{
+		return this.minOutput;
+	}
 
-  /**
-   * Get the maximum allowed output for the controller.
-   *
-   * @return maximum output
-   */
-  public double getMaxOutput() {
-    return this.maxOutput;
-  }
+	/**
+	 * Set the minimum allowed output for the controller.
+	 *
+	 * @param newMinOutput the minimum output of the controller
+	 */
+	public void setMinOutput(double newMinOutput)
+	{
+		this.minOutput = newMinOutput;
+	}
 
-  /**
-   * Set the maximum allowed output for the controller.
-   *
-   * @param newMaxOutput the maximum output of the controller
-   */
-  public void setMaxOutput(double newMaxOutput) {
-    this.maxOutput = newMaxOutput;
-  }
+	/**
+	 * Get the maximum allowed output for the controller.
+	 *
+	 * @return maximum output
+	 */
+	public double getMaxOutput()
+	{
+		return this.maxOutput;
+	}
 
-  /**
-   * Get the maximum allowed proportional component for the controller.
-   *
-   * @return maximum proportional component
-   */
-  public double getMaxProportional() {
-    return this.maxProportional;
-  }
+	/**
+	 * Set the maximum allowed output for the controller.
+	 *
+	 * @param newMaxOutput the maximum output of the controller
+	 */
+	public void setMaxOutput(double newMaxOutput)
+	{
+		this.maxOutput = newMaxOutput;
+	}
 
-  /**
-   * Set the maximum allowed proportional component for the controller.
-   *
-   * @param newMaxProportional the maximum proportional component
-   */
-  public void setMaxProportional(double newMaxProportional) {
-    this.maxProportional = newMaxProportional;
-  }
+	/**
+	 * Get the maximum allowed proportional component for the controller.
+	 *
+	 * @return maximum proportional component
+	 */
+	public double getMaxProportional()
+	{
+		return this.maxProportional;
+	}
 
-  /**
-   * Get the maximum allowed integral component for the controller.
-   *
-   * @return maximum integral component
-   */
-  public double getMaxIntegral() {
-    return this.maxIntegral;
-  }
+	/**
+	 * Set the maximum allowed proportional component for the controller.
+	 *
+	 * @param newMaxProportional the maximum proportional component
+	 */
+	public void setMaxProportional(double newMaxProportional)
+	{
+		this.maxProportional = newMaxProportional;
+	}
 
-  /**
-   * Set the maximum allowed integral component for the controller.
-   *
-   * @param newMaxIntegral the maximum integral component
-   */
-  public void setMaxIntegral(double newMaxIntegral) {
-    this.maxIntegral = newMaxIntegral;
-  }
+	/**
+	 * Get the maximum allowed integral component for the controller.
+	 *
+	 * @return maximum integral component
+	 */
+	public double getMaxIntegral()
+	{
+		return this.maxIntegral;
+	}
 
-  /**
-   * Get the maximum allowed derivitive component for the controller.
-   *
-   * @return maximum derivitive component
-   */
-  public double getMaxDerivitive() {
-    return this.maxDerivitive;
-  }
+	/**
+	 * Set the maximum allowed integral component for the controller.
+	 *
+	 * @param newMaxIntegral the maximum integral component
+	 */
+	public void setMaxIntegral(double newMaxIntegral)
+	{
+		this.maxIntegral = newMaxIntegral;
+	}
 
-  /**
-   * Set the maximum allowed derivitive component for the controller.
-   *
-   * @param newMaxDerivitive the maximum derivitive component
-   */
-  public void setMaxDerivitive(double newMaxDerivitive) {
-    this.maxDerivitive = newMaxDerivitive;
-  }
+	/**
+	 * Get the maximum allowed derivitive component for the controller.
+	 *
+	 * @return maximum derivitive component
+	 */
+	public double getMaxDerivitive()
+	{
+		return this.maxDerivitive;
+	}
 
-  /**
-   * Get the proportional gain of the controller.
-   *
-   * @return proportional gain
-   */
-  public double getProportionalGain() {
-    return this.proportionalGain;
-  }
+	/**
+	 * Set the maximum allowed derivitive component for the controller.
+	 *
+	 * @param newMaxDerivitive the maximum derivitive component
+	 */
+	public void setMaxDerivitive(double newMaxDerivitive)
+	{
+		this.maxDerivitive = newMaxDerivitive;
+	}
 
-  /**
-   * Set the proportional gain of the controller.
-   *
-   * @param newProportionalGain the proportional gain
-   */
-  public void setProportionalGain(double newProportionalGain) {
-    this.proportionalGain = newProportionalGain;
-  }
+	/**
+	 * Get the proportional gain of the controller.
+	 *
+	 * @return proportional gain
+	 */
+	public double getProportionalGain()
+	{
+		return this.proportionalGain;
+	}
 
-  /**
-   * Get the integral gain of the controller.
-   *
-   * @return integral gain
-   */
-  public double getIntegralGain() {
-    return this.integralGain;
-  }
+	/**
+	 * Set the proportional gain of the controller.
+	 *
+	 * @param newProportionalGain the proportional gain
+	 */
+	public void setProportionalGain(double newProportionalGain)
+	{
+		this.proportionalGain = newProportionalGain;
+	}
 
-  /**
-   * Get the total integral of the controller.
-   *
-   * @return integral
-   */
-  public double getIntegral() {
-    return this.integral;
-  }
+	/**
+	 * Get the integral gain of the controller.
+	 *
+	 * @return integral gain
+	 */
+	public double getIntegralGain()
+	{
+		return this.integralGain;
+	}
 
-  /**
-   * Set the integral gain of the controller.
-   *
-   * @param newIntegralGain the integral gain
-   */
-  public void setIntegralGain(double newIntegralGain) {
-    this.integralGain = newIntegralGain;
-  }
+	/**
+	 * Get the total integral of the controller.
+	 *
+	 * @return integral
+	 */
+	public double getIntegral()
+	{
+		return this.integral;
+	}
 
-  /**
-   * Get the derivitive gain of the controller.
-   *
-   * @return derivitive gain
-   */
-  public double getDerivitiveGain() {
-    return this.derivitiveGain;
-  }
+	/**
+	 * Set the integral gain of the controller.
+	 *
+	 * @param newIntegralGain the integral gain
+	 */
+	public void setIntegralGain(double newIntegralGain)
+	{
+		this.integralGain = newIntegralGain;
+	}
 
-  /**
-   * Set the derivitive gain of the controller.
-   *
-   * @param newDerivitiveGain the derivitive gain
-   */
-  public void setDerivitiveGain(double newDerivitiveGain) {
-    this.derivitiveGain = newDerivitiveGain;
-  }
+	/**
+	 * Get the derivitive gain of the controller.
+	 *
+	 * @return derivitive gain
+	 */
+	public double getDerivitiveGain()
+	{
+		return this.derivitiveGain;
+	}
 
-  /**
-   * Get the last computed error value
-   * 
-   * @return last computed error
-   */
-  public double getError() {
-    return this.currentError;
-  }
+	/**
+	 * Set the derivitive gain of the controller.
+	 *
+	 * @param newDerivitiveGain the derivitive gain
+	 */
+	public void setDerivitiveGain(double newDerivitiveGain)
+	{
+		this.derivitiveGain = newDerivitiveGain;
+	}
 
-  /**
-   * Reset values to 0.
-   */
-  public void resetValues() {
-    this.proportional = 0;
-    this.integral = 0;
-    this.derivitive = 0;
-    this.currentError = 0;
-    this.pastError = 0;
-  }
+	/**
+	 * Get the last computed error value
+	 * 
+	 * @return last computed error
+	 */
+	public double getError()
+	{
+		return this.currentError;
+	}
+
+	/**
+	 * Reset values to 0.
+	 */
+	public void resetValues()
+	{
+		this.proportional = 0;
+		this.integral = 0;
+		this.derivitive = 0;
+		this.currentError = 0;
+		this.pastError = 0;
+	}
 }
---
+++ src/utilities/CopyPastAutonomous.java
@@ -0,0 +1,170 @@
+package utilities;
+
+public class CopyPastAutonomous
+{
+	private final ConfigurablePID drivetrainHeadingPID;
+	private final ConfigurablePID drivetrainSpeedPID;
+
+	private double[] pos = { -9999, -9999 };
+
+	private double currentRightMotorPosition;
+	private double currentLeftMotorPosition;
+
+	private double drivePower;
+	private double steeringPower;
+
+	private double currentHeading;
+	private double previousHeading;
+	private double headingError;
+	private double headingRate;
+
+	private double previousRightMotorPosition;
+	private double previousLeftMotorPosition;
+	private double distanceTraveledLeft;
+	private double distanceTraveledRight;
+	private double distanceTraveled;
+	private double distanceError;
+	private double robotX;
+	private double robotY;
+
+	private double targetX;
+	private double targetY;
+	private double targetHeading;
+
+	public CopyPastAutonomous(ConfigurablePID[] PIDArray)
+	{
+		this.robotY = 0;
+		this.robotX = 0;
+		this.targetX = 0;
+		this.targetY = 0;
+		this.targetHeading = 0;
+
+		this.drivetrainHeadingPID = PIDArray[0];
+		this.drivetrainSpeedPID = PIDArray[1];
+	}
+
+	public void setArcadeDrive(double forward, double turn)
+	{
+
+	}
+
+	public void setSwerveDrive(double forward, double turn)
+	{
+
+	}
+
+	public void drivetrainPositionIntegration(double leftMotorEncoderPos, double rightMotorEncodePos, double yaw)
+	{
+		currentLeftMotorPosition = leftMotorEncoderPos / PastaConstants.ENCODER_ROTATION_UNITS;
+		currentRightMotorPosition = rightMotorEncodePos / PastaConstants.ENCODER_ROTATION_UNITS;
+
+		currentHeading = yaw;
+
+		currentHeading = Math.toRadians(currentHeading);
+
+		distanceTraveledLeft = PastaConstants.DRIVETRAIN_ROTATION_DISTANCE_RATIO * PastaConstants.DRIVETRAIN_GEAR_RATIO
+				* (currentLeftMotorPosition - previousLeftMotorPosition);
+		distanceTraveledRight = PastaConstants.DRIVETRAIN_ROTATION_DISTANCE_RATIO * PastaConstants.DRIVETRAIN_GEAR_RATIO
+				* (currentRightMotorPosition - previousRightMotorPosition);
+		distanceTraveled = (distanceTraveledLeft + distanceTraveledRight) / 2;
+
+		previousLeftMotorPosition = currentLeftMotorPosition;
+		previousRightMotorPosition = currentRightMotorPosition;
+
+		robotX = robotX + (Math.cos(currentHeading) * distanceTraveled);
+		robotY = robotY + (Math.sin(currentHeading) * distanceTraveled);
+	}
+
+	public void driveForwardTo(double waypointX, double waypointY)
+	{
+
+	}
+
+	public void driveBackwardsTo(double waypointX, double waypointY)
+	{
+
+	}
+
+	public double[] getPos()
+	{
+		pos[0] = robotX;
+		pos[1] = robotY;
+		return pos;
+	}
+
+	/**
+	 * DriveTrain drives to the specified waypoint No longer functions, uses methods
+	 * no longer supported
+	 * 
+	 * @param waypointX
+	 * @param waypointY
+	 * @param driveBackwards
+	 * @return if it has reached a waypoint
+	 * @deprecated
+	 */
+	public boolean setDriveToWaypoint(double waypointX, double waypointY, boolean driveBackwards)
+	{
+		this.targetX = waypointX;
+		this.targetY = waypointY;
+		if (!driveBackwards)
+		{
+			this.targetHeading = Math.toDegrees(Math.atan2(this.targetY - this.robotY, this.targetX - this.robotX));
+		} else
+		{
+			this.targetHeading = Math
+					.toDegrees(Math.atan2(-(this.targetY - this.robotY), -(this.targetX - this.robotX)));
+		}
+		// this.currentHeading = (double) this.navX.getYaw();
+		this.headingRate = this.currentHeading - this.previousHeading;
+		this.headingError = this.targetHeading - this.currentHeading;
+		this.previousHeading = this.currentHeading;
+
+		this.distanceError = Math
+				.sqrt(Math.pow(this.targetY - this.robotY, 2) + Math.pow(this.targetX - this.robotX, 2));
+
+		this.steeringPower = this.drivetrainHeadingPID.runVelocityPID(this.targetHeading, this.currentHeading,
+				this.headingRate);
+
+		if (Math.abs(this.headingError) < PastaConstants.MAX_DRIVE_HEADING_ERROR)
+		{
+			this.drivePower = this.drivetrainSpeedPID.runPID(0, -this.distanceError);
+			if (driveBackwards)
+			{
+				this.drivePower = -this.drivePower;
+			}
+		} else
+		{
+			this.drivePower = 0;
+		}
+		if (hasReachedWaypoint())
+		{
+			// stopMotors();
+		} else
+		{
+			this.setArcadeDrive(this.drivePower, this.steeringPower);
+		}
+		return hasReachedWaypoint();
+	}
+
+	public boolean pointAtWaypoint(double waypointX, double waypointY)
+	{
+		return false;
+
+	}
+
+	public void pointAtAngle(double targetAngle)
+	{
+
+	}
+
+	public boolean hasReachedWaypoint()
+	{
+		return Math.abs(this.distanceError) < PastaConstants.MAX_WAYPOINT_ERROR;
+	}
+
+	public void setPos(double x, double y)
+	{
+		this.robotX = x;
+		this.robotY = y;
+	}
+}
