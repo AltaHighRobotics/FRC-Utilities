46295108914429c673b9054ee7ab1f4f492e3881
Braden Byrnes <56095224+Cairezn@users.noreply.github.com>
2022-05-08 16:07:06 -0600

    update panel to add mouse implementation

--- CoordinateFinder/src/coordinate/view/Panel.java
+++ CoordinateFinder/src/coordinate/view/Panel.java
@@ -1,6 +1,80 @@
 package coordinate.view;
 
-public class Panel
-{
+import coordinate.controller.Controller;
 
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Panel extends JPanel
+{
+	private Controller app;
+	private SpringLayout layout;
+	private JLabel xLabel;
+	private JLabel yLabel;
+
+	public Panel(Controller app)
+	{
+		super();
+		this.app = app;
+		this.layout = new SpringLayout();
+		this.xLabel = new JLabel("x: ");
+		this.yLabel = new JLabel("y: ");
+
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+
+	private void setupPanel()
+	{
+
+	}
+
+	private void setupLayout()
+	{
+
+	}
+
+	private void setupListeners()
+	{
+		this.addMouseListener(new MouseListener()
+		{
+			public void mousePressed(MouseEvent press)
+			{
+			}
+
+			public void mouseReleased(MouseEvent release)
+			{
+			}
+
+			public void mouseEntered(MouseEvent enter)
+			{
+			}
+
+			public void mouseExited(MouseEvent exit)
+			{
+			}
+
+			public void mouseClicked(MouseEvent click)
+			{
+				updatePosition(click);
+			}
+		});
+	}
+
+	private void updatePosition(MouseEvent currentEvent)
+	{
+		xLabel.setText("x: " + currentEvent.getX());
+		yLabel.setText("y: " + currentEvent.getY());
+	}
+
+	private void mouseDetails(String name, MouseEvent currentEvent)
+	{
+		if (currentEvent.getButton() == MouseEvent.BUTTON1)
+		{
+
+		}
+	}
 }
